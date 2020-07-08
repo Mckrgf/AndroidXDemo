17eac025188482f525e1bf391ae4914b30b1bc18
姚冰 <1124507487@qq.com>
2020-07-09 00:28:55 +0800

    静态变量导致内存泄漏 --- 写出几种内存泄漏的方法 集成leak canary

--- app/src/main/java/com/example/androidxdemo/fragment/MemoryLeakUseCaseFragment.kt
+++ app/src/main/java/com/example/androidxdemo/fragment/MemoryLeakUseCaseFragment.kt
@@ -6,6 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.androidxdemo.R
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.fragment_memory_leak_use_case.*
 
 class MemoryLeakUseCaseFragment : BaseFragment() {
     override fun onCreateView(
@@ -18,8 +20,25 @@ class MemoryLeakUseCaseFragment : BaseFragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        getArgs()
+
+        tv_use_case_a.setOnClickListener {
+            staticView = View(activity)
+            Snackbar.make(requireView(), "已经造成泄露", Snackbar.LENGTH_LONG).show()
+        }
+    }
+
+    /**
+     * fragment之间传递参数
+     */
+    private fun getArgs() {
         val aaa = arguments?.get("name")
+
         val bbb = arguments?.get("age")
-        Log.d(TAG,"aaa : $aaa + bbb : $bbb")
+        Log.d(TAG, "aaa : $aaa + bbb : $bbb")
+    }
+
+    companion object{
+        lateinit var staticView:View
     }
 }
