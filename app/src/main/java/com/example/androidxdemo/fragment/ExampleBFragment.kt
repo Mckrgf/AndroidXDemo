42b83f5a682577e31efb57a1f30f2904cb90df3e
yaobing <1124507487@qq.com>
2020-07-13 17:17:37 +0800

    修改fragment的view逻辑

--- app/src/main/java/com/example/androidxdemo/fragment/ExampleBFragment.kt
+++ app/src/main/java/com/example/androidxdemo/fragment/ExampleBFragment.kt
@@ -5,18 +5,26 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
 import com.example.androidxdemo.R
+import kotlinx.android.synthetic.main.fragment_example_b.*
 
-class ExampleBFragment : BaseFragment(){
+class ExampleBFragment : BaseFragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+
         return inflater.inflate(R.layout.fragment_example_b, container, false)
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
+        bt_change.setOnClickListener {
+            Navigation.findNavController(it).navigate(
+                R.id.action_exampleBFragment_to_exampleCFragment
+            )
+        }
     }
 }
